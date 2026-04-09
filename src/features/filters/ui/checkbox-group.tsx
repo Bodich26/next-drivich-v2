0151c603a1d9ed95ec1b97d32cff26061e44932e
Bodich <4634815@gmail.com>
2026-04-09 13:44:46 +0300

    Import fix.

--- src/features/filters/ui/checkbox-group.tsx
+++ src/features/filters/ui/checkbox-group.tsx
@@ -1,73 +1,73 @@
-"use client";
+// "use client";
 
-import React from "react";
-import { useSet } from "react-use";
-import { FilterCheckbox, FilterCheckboxProps } from "./filters-checkbox";
+// import React from "react";
+// import { useSet } from "react-use";
+// import { FilterCheckbox, FilterCheckboxProps } from "./filters-checkbox";
 
-type Item = FilterCheckboxProps;
+// type Item = FilterCheckboxProps;
 
-type Props = {
-  title: string;
-  items: Item[];
-  defaultItems: Item[];
-  limit?: number;
-  onChange?: (values: string[]) => void;
-  defaultValue?: string[];
-  className?: string;
-};
-export const CheckboxGroup = ({
-  title,
-  items,
-  defaultItems,
-  limit = 5,
-  className,
-  onChange,
-  defaultValue,
-}: Props) => {
-  const [showAll, setShowAll] = React.useState(false);
-  const [selected, { add, toggle }] = useSet<string>(new Set([]));
+// type Props = {
+//   title: string;
+//   items: Item[];
+//   defaultItems: Item[];
+//   limit?: number;
+//   onChange?: (values: string[]) => void;
+//   defaultValue?: string[];
+//   className?: string;
+// };
+// export const CheckboxGroup = ({
+//   title,
+//   items,
+//   defaultItems,
+//   limit = 5,
+//   className,
+//   onChange,
+//   defaultValue,
+// }: Props) => {
+//   const [showAll, setShowAll] = React.useState(false);
+//   const [selected, { add, toggle }] = useSet<string>(new Set([]));
 
-  const onCheckedChange = (value: string) => {
-    toggle(value);
-  };
+//   const onCheckedChange = (value: string) => {
+//     toggle(value);
+//   };
 
-  React.useEffect(() => {
-    if (defaultValue) {
-      defaultValue.forEach(add);
-    }
-  });
+//   React.useEffect(() => {
+//     if (defaultValue) {
+//       defaultValue.forEach(add);
+//     }
+//   });
 
-  React.useEffect(() => {
-    onChange?.(Array.from(selected));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selected]);
+//   React.useEffect(() => {
+//     onChange?.(Array.from(selected));
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [selected]);
 
-  return (
-    <div className={className}>
-      <p className="font-medium text-lg mb-3">{title}</p>
-      <div className="flex flex-col gap-2 max-h-96 overflow-auto scrollbar">
-        {(showAll ? items : defaultItems || items).map((item) => (
-          <FilterCheckbox
-            onCheckedChange={() => onCheckedChange(item.value)}
-            checked={selected.has(item.value)}
-            key={String(item.value)}
-            value={item.value}
-            text={item.text}
-            endAdornment={item.endAdornment}
-          />
-        ))}
-      </div>
+//   return (
+//     <div className={className}>
+//       <p className="font-medium text-lg mb-3">{title}</p>
+//       <div className="flex flex-col gap-2 max-h-96 overflow-auto scrollbar">
+//         {(showAll ? items : defaultItems || items).map((item) => (
+//           <FilterCheckbox
+//             onCheckedChange={() => onCheckedChange(item.value)}
+//             checked={selected.has(item.value)}
+//             key={String(item.value)}
+//             value={item.value}
+//             text={item.text}
+//             endAdornment={item.endAdornment}
+//           />
+//         ))}
+//       </div>
 
-      {items.length > limit && (
-        <div className={showAll ? "" : ""}>
-          <button
-            onClick={() => setShowAll(!showAll)}
-            className="text-primary mt-3"
-          >
-            {showAll ? "Hide" : "+ Show All"}
-          </button>
-        </div>
-      )}
-    </div>
-  );
-};
+//       {items.length > limit && (
+//         <div className={showAll ? "" : ""}>
+//           <button
+//             onClick={() => setShowAll(!showAll)}
+//             className="text-primary mt-3"
+//           >
+//             {showAll ? "Hide" : "+ Show All"}
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
